1706ec774f56642f23ad73a355108d514e1baf0c
suancaixianyu <113163361+suancaixianyu@users.noreply.github.com>
2022-09-26 16:01:00 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 function userAgent() {
 	if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
   	// 当前设备是移动设备
-	var url = "suancaixianyu.github.io";    
+	var url = "home.html";    
 	$(location).attr('href',url);
 	}
 	$("#home").show();
